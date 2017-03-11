be18622665eef3b6768ae4eb1b992588c137dd16
Stath Vaille <stathvaille@gmail.com>
2017-03-11 22:11:53 +0000

    placeholder values for imported items

--- src/app/importSuggestionTable/ImportSuggestion.service.ts
+++ src/app/importSuggestionTable/ImportSuggestion.service.ts
@@ -1,5 +1,22 @@
 export class ImportSuggestionService {
-  getImportSuggestions() : string[] {
-      return ["import suggestion 1", "import suggestion 2"]
+  getImportSuggestions() : any[] {
+      return [{
+                  itemName: "test item",
+                  itemVolume: 40,
+                  minPriceInSource: 100.3,
+                  minPriceInDestination: 234.2,
+                  volRemainingInDestination: 7,
+                  distinctMarketOrdersInDestination: 34,
+                  numberSoldInDestinationPerDay: 3
+              },
+              {
+                  itemName: "test item",
+                  itemVolume: 40,
+                  minPriceInSource: 100.3,
+                  minPriceInDestination: 234.2,
+                  volRemainingInDestination: 7,
+                  distinctMarketOrdersInDestination: 34,
+                  numberSoldInDestinationPerDay: 3
+              }]
   }
 }
